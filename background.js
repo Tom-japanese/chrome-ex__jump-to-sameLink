2b37fd6fb7934161fb055df1c742de528de43e80
Tom <haveto.oshigoto@gmail.com>
2021-06-09 22:10:07 +0900

    first commit

---
+++ background.js
@@ -0,0 +1,36 @@
+﻿function toggleMuteState(tabId) {
+  chrome.tabs.get(tabId, async (tab) => {
+    let muted = !tab.mutedInfo.muted;
+    await chrome.tabs.update(tabId, { muted });
+    console.log(`Tab ${tab.id} is ${ muted ? 'muted' : 'unmuted' }`);
+  });
+}
+chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,thisTab){ 
+	
+	// console.log(tabId);
+	// console.log(changeInfo);
+	// console.log(thisTab);
+  // これから開くタブ（loading）既に開いているタブ（complete）
+  if(changeInfo.status == "loading"){
+    // 遷移先のURLを取得
+    // console.log(thisTab.url);
+    // 全てのタブの中から同じタブがあるかどうか確認する
+    chrome.tabs.query({currentWindow:true},function(tabs){
+      console.log(tabs);// 現在開いているタブの情報を取得できる
+      for(i = 0; i < tabs.length;i++){
+        if(thisTab.url == tabs[i].url && thisTab.id!=tabs[i].id){
+          // 該当のタブに飛ぶ
+          chrome.tabs.update(tabs[i].id,{highlighted:true},null);
+          // 開いているタブを削除
+          chrome.tabs.remove(thisTab.id,null);
+          break;		
+        }
+      }
+    });
+
+  }
+
+
+
+
+});
---
+++ popup.js
@@ -0,0 +1,46 @@
+﻿
+
+var title = chrome.tabs.getSelected(function(tab) {  // 2 
+      return tab.title;
+  });
+console.log(title);
+
+
+/* ==================================================
+コメントアウト
+==================================================  */
+
+function Save() {
+  // タイムスタンプ作成
+  var date = new Date() ;
+  var a = date.getTime() ;
+
+  var message = document.getElementById('input_message').value;
+  var array = [];
+  array.push(message);
+  var keyName = 'info_' + a;
+  chrome.tabs.getSelected(function(tab) { 
+    array.push(tab.title);
+    array.push(tab.url);
+    console.log(keyName);
+    chrome.storage.local.set({'info_': array}, function () { });
+
+  });
+  document.getElementById('input_message').value = message;
+}
+
+document.getElementById('save_button').addEventListener('click', Save);　　// 保存ボタン（save_button）がクリックされたらSave関数を実行
+
+
+
+/* ==================================================
+コメントアウト
+==================================================  */
+
+// function Load() {
+//   chrome.storage.local.get('Alertmsg', function (items) {
+//     document.getElementById('input_message').value = items.Alertmsg;  // Alertmsgキーと対に記録された文字列を、idがinput_messageのテキストボックスに出力
+//   });
+// }
+
+// document.addEventListener('DOMContentLoaded', Load);  // オプションページ（options.html）の読み込みと解析が完了したらLoad関数を実行
